4d81c165a121e585bacd57b33e1c8609f47fb04e
Worksduvi <125094799+Worksduvi@users.noreply.github.com>
2026-02-12 04:22:00 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vico-pocket-v9.2';
+const CACHE_NAME = 'vico-pocket-v9.3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', e => {
