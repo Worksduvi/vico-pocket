a8b0b38dd7c543496075c4aa21c9e0f23157f5db
Worksduvi <125094799+Worksduvi@users.noreply.github.com>
2026-02-12 16:53:20 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vico-elite-v12';
+const CACHE_NAME = 'vico-pocket-v12.5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))));
