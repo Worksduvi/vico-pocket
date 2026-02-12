59e6933c09218a28a54d65ac60a06499b9e9912b
Worksduvi <125094799+Worksduvi@users.noreply.github.com>
2026-02-12 16:10:39 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,5 @@
-const CACHE_NAME = 'vico-elite-v10';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png'
-];
-
-// Instalación y limpieza de caches antiguos
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-// Activación: Borra cualquier cache que no sea la v10
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+const CACHE_NAME = 'vico-pocket-v10.5';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
