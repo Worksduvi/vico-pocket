ca1ef70712bbe324afcb7f8540cea9e1524df788
Worksduvi <125094799+Worksduvi@users.noreply.github.com>
2026-02-12 15:41:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,4 @@
-const CACHE_NAME = 'vico-pocket-v9.9';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.png', 'https://cdn.tailwindcss.com'];
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+const CACHE_NAME = 'vico-pocket-v9.9.8';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))); });
